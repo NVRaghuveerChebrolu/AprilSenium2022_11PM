5ae30905ff41b389f2868a55eff6f208b7ac13d1
RaghuveerChebrolu <raghuveerchebrolu@gmail.com>
2022-04-29 00:14:31 +0530

    28April 2022 Java basics and control statements

---
+++ src/test/java/com/JavaBasics/Operators.java
@@ -0,0 +1,16 @@
+package com.JavaBasics;
+
+public class Operators {
+	public static void main(String[] args) {
+		int i =7,j=8;
+		System.out.println(i++);
+		System.out.println(++i);
+		System.out.println(i--);
+		System.out.println(--i);
+		
+		System.out.println(++j + j++);
+		System.out.println(j++ + ++j);
+		System.out.println(--j - j--);
+		System.out.println(j-- - j++);
+	}
+}
